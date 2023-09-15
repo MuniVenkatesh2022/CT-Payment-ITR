8e78c39bba752ff95a8b9932a99a501122e98f9e
Muni B <bathalamunivenkatesh899@gmail.com>
2023-09-15 15:41:22 +0530

    added helloWord method

--- src/main/java/com/cpt/payments/controller/PaymentTestController.java
+++ src/main/java/com/cpt/payments/controller/PaymentTestController.java
@@ -1,5 +1,6 @@
 package com.cpt.payments.controller;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,11 @@ public class PaymentTestController {
 	@GetMapping("/payment")
 	public String getPayment() {
 		return "Payment working";
+	}
+	
+	@GetMapping("/getHello")
+	public ResponseEntity<?> helloWorld(){
+		return ResponseEntity.ok().body("hello world");
 	}
 
 }
